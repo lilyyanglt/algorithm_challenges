8e8db06dac19312e4bf4f01696ed2f4847b031bc
Lily <lilyyanglt@gmail.com>
2020-08-29 15:36:40 -0700

    Added an update to #876 solution

--- leetcode/solutions/cpp/876_middleOfTheLinkedList.cpp
+++ leetcode/solutions/cpp/876_middleOfTheLinkedList.cpp
@@ -1,9 +1,24 @@
-// Link: https://leetcode.com/problems/middle-of-the-linked-list/
-// Problem #: 876
-// Author: Lily Yang
-// Date: June 26, 2020
-// Notes: (https://github.com/lilyyanglt/leetcode/blob/master/notes/876.md)
+/* Link: https://leetcode.com/problems/middle-of-the-linked-list/
+* Problem #: 876
+* Author: Lily Yang
+* Date: June 26, 2020
+* Notes: (https://github.com/lilyyanglt/leetcode/blob/master/notes/876.md)
 
+Update on Aug 29, 2020
+
+Approach:
+- using slow and fast pointer for this question. 
+- if the root is null, return - there's no middle
+- if the root->next null - ie it only has 1 node, return node->val since that is the middle val
+- else, A <- is the slow pointer, B <- fast pointer
+- both starting at root 
+- while B->next != null, I will assign A <- A->next, and B = B->next->next
+  - if B is null, return A->val immediately
+
+Time Complexity: O(n/2) - n is the number of nodes
+Space: O(1) - the memory required does not grow with the input size
+
+*/
 #include <iostream>
 
 // Definition for singly-linked list.
@@ -17,7 +32,7 @@
  };
 
 
-class Solution {
+class Solution1 {
 public:
     ListNode* middleNode(ListNode* head) {
         ListNode* slow = head;
@@ -31,6 +46,29 @@ public:
         }
         
         return slow;
+    }
+};
+
+// updated on Aug 29, 2020 - Updated solution
+class Solution2 {
+  public:
+     int solve(LLNode* node) {
+        // empty linkedlist
+        if (!node) return -1;
+        if (!node->next) return node->val;
+        
+        LLNode* A = node;
+        LLNode* B = node;
+        
+        while (B->next!=nullptr) {
+            A = A->next;
+            B = B->next->next;
+            if (B == nullptr) {
+                return A->val;
+            }
+        }
+        
+        return A->val;
     }
 };
 
